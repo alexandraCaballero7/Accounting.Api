68a649142b87ae4daf59668e18cc46b6b29da20e
Alexandra Caballero <alxndra.06.10@gmail.com>
2026-02-28 23:13:40 -0800

    Implement Employee and Voucher management controllers and remove WeatherForecast files

---
+++ Accounting.Api/Controllers/EmployeesController.cs
@@ -0,0 +1,55 @@
+﻿
+using Accounting.Application.Employees.Commands.Add;
+using Accounting.Application.Employees.Commands.Delete;
+using Accounting.Application.Employees.Commands.Update;
+using Accounting.Application.Employees.Queries;
+using Accounting.Core.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController(ISender sender) : ControllerBase
+    {
+
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAllEmployeeAsync()
+        {
+            var result = await sender.Send(new GetAllEmployeesQuery());
+            return Ok(result);
+        }
+
+
+        [HttpGet("{EmployeeId}")]
+        public async Task<IActionResult> GetAllEmployeeByIdAsync(int EmployeeId)
+        {
+            var result = await sender.Send(new GetEmployeeByIdQuery(EmployeeId));
+            return Ok(result);
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> AddEmployeeAsync([FromBody] AddEmployeeRequest employee)
+        {
+            var result = await sender.Send(new AddEmployeeCommand(employee));
+            return Ok(result);
+        }
+
+        [HttpPut("{EmployeeId}")]
+        public async Task<IActionResult> UpdateEmployeeAsync(int EmployeeId, [FromBody] UpdateEmployeRequest employee)
+        {
+            var result = await sender.Send(new UpdateEmployeeCommand(EmployeeId,employee));
+            return Ok(result);
+        }
+
+        [HttpDelete("{EmployeeId}")]
+        public async Task<IActionResult> DeleteEmployeeAsync(int EmployeeId)
+        {
+            var result = await sender.Send(new DeleteEmployeeCommand(EmployeeId));
+            return Ok(result);
+        }
+    }
+}
---
+++ Accounting.Api/Controllers/VouchersController.cs
@@ -0,0 +1,28 @@
+﻿using Accounting.Application.Employees.Commands.Add;
+using Accounting.Application.Vouchers.Commands.Add;
+using Accounting.Application.Vouchers.Commands.Delete;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accounting.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VouchersController(ISender sender) : ControllerBase
+    {
+        [HttpPost("")]
+        public async Task<IActionResult> AddVoucherAsync([FromBody] AddVoucherRequest voucher)
+        {
+            var result = await sender.Send(new AddVouchersCommand(voucher));
+            return Ok(result);
+        }
+
+        [HttpDelete("{VoucherId}")]
+        public async Task<IActionResult> DeleteVoucherByIdAsync(int VoucherId)
+        {
+            var result = await sender.Send(new DeleteVouchersCommand(VoucherId));
+            return Ok(result);
+        }
+    }
+}
